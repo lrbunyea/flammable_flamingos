81e7579f7be1eca6c42d358aea65c265791ed28f
theoncomingfish <mjszpunar@wpi.edu>
2017-11-14 23:22:07 -0500

    woo sleeping

---
+++ Assets/Scripts/Battle/StatusEffect/SleepStatus.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SleepStatus : StatusEffect {
+
+    public int turns;
+
+    public override void PreTurn(Stats stats, Turn turn)
+    {
+        int random = Random.Range(0, 1);
+        if(random == 0)
+        {
+            //TODO: use function to end turn
+
+        }
+    }
+
+    public override void PostTurn(Stats stats, Turn turn)
+    {
+        if (turns != 3)
+        {
+            turns++;
+        }
+        else
+        {
+            Complete = true;
+        }
+    }
+}
